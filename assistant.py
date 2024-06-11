12064d2b26d4deab0644a709e3eb5c7d3bc2c0d0
Bruno STORDEUR <bstordeur-consultant@cartes-bancaires.com>
2024-06-11 17:53:54 +0200

    some code added

---
+++ assistant.py
@@ -0,0 +1,18 @@
+import base64
+from threading import Lock, Thread
+
+import cv2
+import openai
+from cv2 import VideoCapture, imencode
+from dotenv import load_dotenv
+from langchain.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.schema.messages import SystemMessage
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
+from pyaudio import PyAudio, paInt16
+from speech_recognition import Microphone, Recognizer, UnknownValueError
+
+load_dotenv()
